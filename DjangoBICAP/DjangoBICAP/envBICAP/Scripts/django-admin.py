295769fee0440065cfeaa32ddf50ae1e40986e8f
GianQuazzo <q.gian99@quarzo.stream>
2020-05-30 15:01:44 +0200

    Creazione ambiente virtuake

---
+++ DjangoBICAP/DjangoBICAP/envBICAP/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\SgozziCoders\repo\BICAP-backend\DjangoBICAP\DjangoBICAP\envBICAP\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
